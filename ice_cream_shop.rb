29dae7ce51fd5e7780e7d27bc24d618559606276
William Holt <wtholt27@gmail.com>
2015-03-30 16:46:43 -0400

    first commit

---
+++ ice_cream_shop.rb
@@ -0,0 +1,31 @@
+class Cone
+
+  def initialize
+    self.name = "Default"
+    self.cost = 0
+    self.id = 0
+  end
+
+  def display_cone
+    puts "#{self.name}" + " " + "-" + " " + "#{self.cost}"
+  end
+  
+end
+
+class Milkshake
+  def initialize
+  end
+end
+
+class Float
+  def initialize
+  end
+end
+
+class Menu
+  def initialize
+  end
+end
+
+class Order
+end

69dcde90da51772400487e4d85f686041c644c4c
William Holt <wtholt27@gmail.com>
2015-04-03 20:48:40 -0400

    working on ice cream challenge

--- ice_cream_shop.rb
+++ ice_cream_shop.rb
@@ -12,8 +12,8 @@ class Item
     self.cost = 0
     self.scoop = 1
     self.flavor = "Default"
-    self.soda = "Default"
-    self.milk = "Default"
+    self.soda = "Not Applicable"
+    self.milk = "Whole"
     self.id = 0
   end
 
@@ -22,8 +22,50 @@ class Item
   end
 
   def ordered_item
-    puts "#{self.name}" + ' ' + "with" + " " + "#{self.scoop}" + " " + "scoop(s)" + " " + "of" + ' ' + "#{self.flavor}" + " " + "$" + "#{self.cost}"
+    puts "#{self.name}" + ' ' + "with" + " " + "#{self.scoop}" + " " + "scoop(s)" + " " + "of" + ' ' + "#{self.flavor}"
+    puts "Milk:" + " " + "#{self.milk}" 
+    puts "Soda:" + " " + "#{self.soda}" 
+    puts "Cost:" + " " + "$" + "#{self.cost}"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   end
+
+  def choose_flavor
+    self.flavor = gets.chomp
+    if self.flavor == "1" 
+      self.flavor = "Vanilla"
+    elsif self.flavor == "2"
+      self.flavor = "Chocolate"
+    elsif self.flavor == "3"
+      self.flavor = "Strawberry"
+    elsif self.flavor == "4"
+      self.flavor = "Peach"
+    elsif self.flavor == "5"
+      self.flavor = "Pistachio"
+    end
+  end
+
+  def choose_soda
+    self.soda = gets.chomp
+    if self.soda == "1"
+      self.soda = "Cherry"
+    elsif self.soda == "2"
+      self.soda = "Root Beer"
+    elsif self.soda == "3"
+      self.soda "Chocolate Cream"
+    end
+  end
+
+  def choose_milk
+    self.milk = gets.chomp
+    if self.milk == "1"
+      self.milk = "Whole"
+    elsif self.milk == "2"
+      self.milk = "2%"
+    elsif self.milk == "3"
+      self.milk = "Skim"
+    end
+  end
+
 end
 
 class Menu
@@ -96,18 +138,31 @@ while game
   new_order = gets.chomp
   new_order
   if new_order == '1'
+    cone = Item.new
+    cone.name = "Ice Cream Cone"
+    cone.cost = 3
     puts "1 or 2 scoops?"
     cone.scoop = gets.chomp
-    puts "What flavor?"
-    cone.flavor = gets.chomp
+    puts "Choose Flavor? 1 for Vanilla, 2 for Chocolate, 3 for Strawberry, 4 for Peach, or 5 for Pistachio"
+    cone.choose_flavor
     order.items.push(cone)
   elsif new_order == '2'
-    puts "What flavor?"
-    milkshake.flavor = gets.chomp
+    milkshake = Item.new
+    milkshake.name = "Milkshake"
+    milkshake.cost = 7
+    puts "Choose Flavor? 1 for Vanilla, 2 for Chocolate, 3 for Strawberry, 4 for Peach, or 5 for Pistachio"
+    milkshake.choose_flavor
+    puts "Choose Milk. 1 for Whole, 2 for 2%, 3 for Skim."
+    milkshake.choose_milk
     order.items.push(milkshake)
   elsif new_order == '3'
-    puts "How many Flavor(s)?"
-    float.flavor = gets.chomp
+    float = Item.new
+    float.name = "Float"
+    float.cost = 5
+    puts "Choose Flavor? 1 for Vanilla, 2 for Chocolate, 3 for Strawberry, 4 for Peach, or 5 for Pistachio"
+    float.choose_flavor
+    puts "Choose a Soda. 1 for Cherry, 2 for Root Beer, 3 for Chocolate Cream."
+    float.choose_soda
     order.items.push(float)
   elsif new_order == '4'
     puts "Your total is" + " " + "$" + "#{order.total}"
